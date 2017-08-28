95d85c5afeccb4fccfd98a3eab9a1e3d6a3217aa
Anthony <cdistc@163.com>
2017-08-29 00:29:58 +0800

    h

--- src/main/java/anthony/apps/carbonmiao/user/web/UserController.java
+++ src/main/java/anthony/apps/carbonmiao/user/web/UserController.java
@@ -10,8 +10,8 @@ public class UserController {
 //    cmd /k "%ConEmuDir%\..\init.bat"  -new_console:p:n:d:%USERPROFILE% &bash
     @RequestMapping("{userId}")
     public String userInfo(@PathVariable("userId") String userId) {
-        System.out.println(userId+"1123e41112");
-        System.out.println(userId+"1123e41112");
+        System.out.println(userId+"11234112");
+        System.out.println(userId+"11234112");
         return null;
     }
 }
