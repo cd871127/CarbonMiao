66393990a56b1256936a5a381849f6447f3263fc
cdistc <cdistc@163.com>
2017-08-31 18:02:50 +0800

    t

--- src/main/java/anthony/apps/carbonmiao/authorization/controller/AuthorizationController.java
+++ src/main/java/anthony/apps/carbonmiao/authorization/controller/AuthorizationController.java
@@ -1,4 +1,9 @@
 package anthony.apps.carbonmiao.authorization.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthorizationController {
+
+
 }
