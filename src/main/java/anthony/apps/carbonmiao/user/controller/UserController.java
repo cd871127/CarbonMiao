ec2e5378e77c88aa7754771c9479645f1838db58
cdistc <cdistc@163.com>
2017-09-08 17:50:51 +0800

    crypt

--- src/main/java/anthony/apps/carbonmiao/user/controller/UserController.java
+++ src/main/java/anthony/apps/carbonmiao/user/controller/UserController.java
@@ -33,6 +33,7 @@ public class UserController {
         return userInfoDTO;
     }
 
+
     @CrossOrigin(origins = SystemConfigParameters.PAGE_SERV, methods = GET)
     @RequestMapping(value = "{userId}", method = GET)
     public UserInfoDTO login(@PathVariable("userId") String userId, @RequestParam("passWord")String passWord, HttpServletResponse response) {
